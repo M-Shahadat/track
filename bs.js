3e965b92f103effcaff668fd6cb654180be0e421
M-Shahadat <www.ovilashkabir@gmail.com>
2022-06-09 00:39:40 +0600

    trying

---
+++ bs.js
@@ -0,0 +1,79 @@
+// let count = 5 - 12
+// let firstBatch = 6
+// let secondBatch = 98
+// let count = firstBatch + secondBatch
+// console.log(count)
+// let myAge = 16 
+// console.log(myAge)
+
+// let myAge = 35
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio 
+
+// console.log(myDogAge)
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+
+// let lapsCompleted = 0
+
+// console.log(lapsCompleted)
+
+
+// function increamentLap()  {
+    
+//     lapsCompleted = lapsCompleted + 1
+
+// }
+
+
+// increamentLap()
+// increamentLap()
+// increamentLap()
+// increamentLap()
+
+// console.log(lapsCompleted)
+
+// let lapsCompleted = 0 
+
+// console.log(lapsCompleted)
+
+// function increamentLap() {
+
+//     lapsCompleted = lapsCompleted + 9
+
+// }
+
+// increamentLap()
+// increamentLap()
+
+//  console.log(lapsCompleted)
+// let countEl = document.getElementById("count-el")
+
+// console.log(countEl)
+
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     countEl.innerText = count
+// }
+
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+    console.log(count)
+}
+
+
